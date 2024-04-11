9dc3011944cc179a3ab0fd6392a298ece97da20a
dgerth5 <davidgerth5@gmail.com>
2024-04-10 21:44:33 -0500

    add in pitching / fielding

--- clean_data.R
+++ clean_data.R
@@ -31,4 +31,58 @@ clean_hit <- juco_hit_data %>%
                           adj_yr %in% jr ~ "JR",
                           adj_yr %in% sr ~ "SR",
                           adj_yr == "IF" ~ Yr,
-                          adj_yr == "OF" ~ Yr))
+                          adj_yr == "OF" ~ Yr)) %>%
+  mutate(across(.cols = 6:20, .fns = ~replace_na(as.numeric(.), 0)))
+
+# ext hit
+
+juco_hit_ext_data <- read_csv("juco_hit_ext_data.csv")
+
+clean_hit_ext <- juco_hit_ext_data %>%
+  filter(!Name %in% c("Totals", "Opponent", "- -")) %>%
+  mutate(adj_yr = if_else(TeamName == "Chipola", Pos, Yr),  # reach out to njcaa and see if this can get updated
+         Year = case_when(adj_yr %in% fr ~ "FR",
+                          adj_yr %in% rfr ~ "r-FR",
+                          adj_yr %in% so ~ "SO",
+                          adj_yr %in% rso ~ "r-SO",
+                          adj_yr %in% jr ~ "JR",
+                          adj_yr %in% sr ~ "SR",
+                          adj_yr == "IF" ~ Yr,
+                          adj_yr == "OF" ~ Yr)) %>%
+  mutate(across(.cols = 6:16, .fns = ~replace_na(as.numeric(.), 0)))
+
+# pitch data
+
+juco_pitch_data <- read_csv("juco_pitch_data.csv")
+
+sapply(juco_pitch_data, class)
+
+clean_pitch <- juco_pitch_data %>%
+  filter(!Name %in% c("Totals", "Opponent", "- -")) %>%
+  mutate(adj_yr = if_else(TeamName == "Chipola", Pos, Yr),  # reach out to njcaa and see if this can get updated
+         Year = case_when(adj_yr %in% fr ~ "FR",
+                          adj_yr %in% rfr ~ "r-FR",
+                          adj_yr %in% so ~ "SO",
+                          adj_yr %in% rso ~ "r-SO",
+                          adj_yr %in% jr ~ "JR",
+                          adj_yr %in% sr ~ "SR",
+                          adj_yr == "IF" ~ Yr,
+                          adj_yr == "OF" ~ Yr)) %>%
+  mutate(across(.cols = c(11,13:17,19,21), .fns = ~replace_na(as.numeric(.), 0)))
+
+# field data
+
+juco_fielding_data <- read_csv("juco_fielding_data.csv")
+
+clean_field <- juco_fielding_data %>%
+  filter(!Name %in% c("Totals", "Opponent", "- -")) %>%
+  mutate(adj_yr = if_else(TeamName == "Chipola", Pos, Yr),  # reach out to njcaa and see if this can get updated
+         Year = case_when(adj_yr %in% fr ~ "FR",
+                          adj_yr %in% rfr ~ "r-FR",
+                          adj_yr %in% so ~ "SO",
+                          adj_yr %in% rso ~ "r-SO",
+                          adj_yr %in% jr ~ "JR",
+                          adj_yr %in% sr ~ "SR",
+                          adj_yr == "IF" ~ Yr,
+                          adj_yr == "OF" ~ Yr)) %>%
+  mutate(across(.cols = 12:17, .fns = ~replace_na(as.numeric(.), 0)))
